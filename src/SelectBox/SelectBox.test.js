4cb73f1fb8e59f76a6e94e3d410d30ca07b5691e
Khalid Williams <khalidwill27@gmail.com>
2020-04-20 17:28:47 -0600

    Write initial test statements

---
+++ src/SelectBox/SelectBox.test.js
@@ -0,0 +1,20 @@
+import SelectBox from './SelectBox';
+import { render } from '@testing-library/react';
+
+
+describe('Select Box', () => {
+  const getUtils = (propsToRender) => {
+    return render(<SelectBox {...propsToRender} />);
+  }
+  test('should be accessible as a combobox', () => {
+    
+  });
+
+  test('should update its value once changed', () => {
+  
+  });
+
+  test('should be able to take in a list of previous values', () => {
+  
+  });
+});
