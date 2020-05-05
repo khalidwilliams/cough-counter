6ce6fcdda29448f979cb7f14da06b491683b435e
khalidwilliams <khalidwill27@gmail.com>
2020-05-04 23:11:42 -0600

    Render basic nav bar component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import './App.css';
+import styled from 'styled-components';
 import CoughForm from '../form/CoughForm/CoughForm';
 import Banner from '../Banner/Banner';
+import NavBar from '../navigation/NavBar/NavBar';
+
+const StyledMain = styled.main`
+  display: grid;
+  grid-template: 
+    'a b'
+  'a b';
+  grid-template-columns: 1fr 4fr
+`
 
 function App() {
   return (
     <>
       <Banner />
-      <CoughForm />
+      <StyledMain>
+        <NavBar />
+        <CoughForm />
+      </StyledMain>
     </>
   );
 }
--- src/components/navigation/NavBar/NavBar.js
+++ src/components/navigation/NavBar/NavBar.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import NavOption from '../NavOption/NavOption';
+
+const StyledNav = styled.nav`
+  display:flex;
+  flex-direction:column;
+`
 
 class NavBar extends Component {
   constructor() {
@@ -7,16 +13,23 @@ class NavBar extends Component {
     this.state = {
       //    If I'm using Router eventually, I don't think I'll need this to be a controlled component - 5/4
   //    currentView: 'New Cough'
+      navOptions: [
+        { name: 'New Cough', path:'/new-cough'},
+        { name: 'Medications', path: '/medications'},
+        { name: 'Account', path: '/account'}
+      ]
     }
   }
 
 
-
   render() {
+  const renderedOptions = this.state.navOptions.map(option => (
+    <NavOption {...option}/>
+  ));
     return (
-      <nav>
-        Navigation
-      </nav>
+      <StyledNav>
+        {renderedOptions}
+      </StyledNav>
     );
   }
 
